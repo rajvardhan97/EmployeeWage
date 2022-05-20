60c5a2c75f7813cf95f27965cdd1158cfde08241
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-20 12:15:05 +0530

    Save the Total Wage for Each Company

---
+++ EmployeeWage/Companies.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Companies
+    {
+        public int FullTime = 8;
+        public int PartTime = 4;
+        public int WageHour = 20;
+        public int maxworkhour = 100;
+        public int maxworkdays = 20;
+        public String CompanyName;
+
+        public Companies(String CompanyName, int WageHour, int FullTime, int PartTime, int maxworkhour, int maxworkdays)
+        {
+            this.CompanyName = CompanyName;
+            this.WageHour = WageHour;
+            this.FullTime = FullTime;
+            this.PartTime = PartTime;
+            this.maxworkhour = maxworkhour;
+            this.maxworkdays = maxworkdays;
+        }
+
+    }
+    class TotalEmployeeWage
+    {
+        public const int EmpAbsent = 0;
+        public const int EmpFullTime = 1;
+        public const int EmpPartTime = 2;
+        public double DailyWage = 0;
+        public double TotalWage = 0;
+        Dictionary<String, Companies> Company = new Dictionary<string, Companies>();
+  
+        public void AddCompany(String CompanyName, int WageHour, int FullTime, int PartTime, int maxworkhour, int maxworkdays)
+        {
+            Companies companies = new Companies(CompanyName, WageHour, FullTime, PartTime, maxworkhour, maxworkdays);
+            Company.Add(CompanyName, companies);
+        }
+
+
+        public void MonthlyWages(string CompanyName)
+        {
+            Random Check = new Random();
+            int days = 1, WorkingHours = 0, TotalHours = 0, Present = 1;
+            int status = Check.Next(0, 2);
+            if (status == Present)
+            {
+
+                if (!Company.ContainsKey(CompanyName))
+                    throw new ArgumentNullException("Company not found");
+                Company.TryGetValue(CompanyName, value: out Companies? companies);
+
+                while (days < companies.maxworkdays && TotalHours <= companies.maxworkhour)
+                {
+
+                    int CheckTime = Check.Next(0, 3);
+                    switch (CheckTime)
+                    {
+                        case EmpAbsent:
+                            WorkingHours = 0;
+                            break;
+                        case EmpFullTime:
+                            WorkingHours = companies.FullTime;
+                            break;
+                        case EmpPartTime:
+                            WorkingHours = companies.PartTime;
+                            break;
+                    }
+                    DailyWage = WorkingHours * companies.WageHour;
+                    TotalWage += DailyWage;
+                    days++;
+                    TotalHours += WorkingHours;
+                }
+                
+                Dictionary<String, double> WagesEmp = new Dictionary<string, double>();
+                WagesEmp.Add(CompanyName, TotalWage);
+             
+                foreach (KeyValuePair<string, double> dict in WagesEmp)
+                {
+                    Console.WriteLine("Company : {0} | Monthly Wage : {1}", dict.Key, dict.Value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static EmployeeWage.Company;
+using static EmployeeWage.Companies;
 
 namespace EmployeeWage
 {
